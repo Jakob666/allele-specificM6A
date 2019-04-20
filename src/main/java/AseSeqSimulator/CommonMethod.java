1ca6650484df6348c0af59192a2300a64dc74ca9
hubs <hubs@mail2.sysu.edu.cn>
2019-04-20 09:58:17 +0800

    UCSC 2bit parser, fragmentation process and common method for sequencing reads generation

---
+++ src/main/java/AseSeqSimulator/CommonMethod.java
@@ -0,0 +1,57 @@
+package AseSeqSimulator;
+
+import GTFComponent.GeneRecord;
+import GTFComponent.TranscriptRecord;
+
+import java.util.*;
+
+public class CommonMethod {
+
+    /**
+     * generate anti-chain for a particular sequence
+     * @param Seq sequence in direction 5' -> 3'
+     * @return anti-chain sequence
+     */
+    public static String AntiChain(String Seq) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < Seq.length(); i++) {
+            char nucleotide = Seq.charAt(i);
+            if (nucleotide == 'A') {
+                sb.insert(0, 'T');
+            } else if (nucleotide == 'T') {
+                sb.insert(0, 'A');
+            } else if (nucleotide == 'C') {
+                sb.insert(0, 'G');
+            } else {
+                sb.insert(0, 'C');
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * find the transcript with longest sequence length of a certain gene
+     * @param geneRecord GeneRecord instance
+     * @return TranscriptRecord instance
+     */
+    public static TranscriptRecord findLongestTranscript(GeneRecord geneRecord) {
+        HashMap<String, TranscriptRecord> transcriptIsoforms = geneRecord.getTranscriptIsoform();
+        int max_Length = 0;
+        TranscriptRecord transcriptRecord = new TranscriptRecord();
+        Set<String> isoformIds = transcriptIsoforms.keySet();
+        int transcriptStart, transcriptEnd;
+        for (String isoformId: isoformIds) {
+            TranscriptRecord record = transcriptIsoforms.get(isoformId);
+            transcriptStart = record.getTranscriptStart();
+            transcriptEnd = record.getTranscriptEnd();
+            int transcript_length = transcriptEnd - transcriptStart;
+            if (transcript_length > max_Length) {
+                transcriptRecord = record;
+                max_Length = transcript_length;
+            }
+        }
+        return transcriptRecord;
+    }
+
+
+}
---
+++ src/main/java/AseSeqSimulator/Fragmentation.java
@@ -0,0 +1,65 @@
+package AseSeqSimulator;
+
+/**
+ * fragment is a sequence cut from a transcript sequence. Generate sequencing reads from it
+ */
+public class Fragmentation {
+    private int fragmentStart, fragmentEnd;
+    private String transcriptFragmentSeq, readSeq;
+    private int fragmentLength;
+
+    /**
+     * constructor
+     * @param transcriptFragmentSeq fragment sequence extract from exon region
+     * @param fragmentStart fragment start point on exon sequence
+     * @param fragmentEnd fragment end point on exon sequence
+     */
+    public Fragmentation(String transcriptFragmentSeq, int fragmentStart, int fragmentEnd){
+        this.transcriptFragmentSeq = transcriptFragmentSeq;
+        this.fragmentStart = fragmentStart;
+        this.fragmentEnd = fragmentEnd;
+        this.fragmentLength = transcriptFragmentSeq.length();
+    }
+
+    public int getFragmentStart(){
+        return this.fragmentStart;
+    }
+
+    public int getFragmentEnd(){
+        return this.fragmentEnd;
+    }
+
+    public String getTranscriptSeq(){
+        return this.transcriptFragmentSeq;
+    }
+
+    public String getReadSeq(){
+        return this.readSeq;
+    }
+
+    public int getFragmentLength() {
+        return this.fragmentLength;
+    }
+
+    /**
+     * generate one sequencing reads based on fragment, the length of which is equals to user input reads
+     * @param strand positive or negative strand
+     * @param readLength read length
+     */
+    public void getSequencingRead(String strand, int readLength){
+        if(strand.equals("+")){
+            if(this.transcriptFragmentSeq.length() > readLength) {
+                this.readSeq = this.transcriptFragmentSeq.substring(0, readLength);
+            }else{
+                this.readSeq = this.transcriptFragmentSeq;
+            }
+        }else{
+            String AntiFragment = CommonMethod.AntiChain(this.transcriptFragmentSeq);
+            if(this.transcriptFragmentSeq.length() > readLength){
+                this.readSeq = AntiFragment.substring(0, readLength);
+            }else{
+                this.readSeq = AntiFragment;
+            }
+        }
+    }
+}
