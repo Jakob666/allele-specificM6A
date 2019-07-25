74f1eb58fbaf03f48d1983933cf4440e46d13244
hubs <hubs@mail2.sysu.edu.cn>
2019-07-25 16:09:48 +0800

    optimized the stability of peak calling process with Rserve

--- src/main/java/meripSeqPeakCalling/PeakCaller.java
+++ src/main/java/meripSeqPeakCalling/PeakCaller.java
@@ -2,10 +2,36 @@ package meripSeqPeakCalling;
 
 import org.apache.commons.cli.*;
 import org.apache.log4j.Logger;
+import org.rosuda.REngine.REXP;
+import org.rosuda.REngine.Rserve.RConnection;
+import org.rosuda.REngine.Rserve.RserveException;
 
 import java.io.*;
 
-public class peakCaller {
+/**
+ * 通过RServe使Java与R进行交互，使用MeTPeak包进行peak calling
+ */
+public class PeakCaller {
+    private String gtfFile, ipBam, inputBam, outputDir, experimentName;
+    private Logger logger;
+
+    /**
+     * Constructor
+     * @param gtfFile GTF文件
+     * @param ipBam IP样本比对结果
+     * @param inputBam  INPUT样本比对结果
+     * @param outputDir 输出文件目录
+     * @param experimentName 实验名称
+     * @param logger log4j对象
+     */
+    public PeakCaller(String gtfFile, String ipBam, String inputBam, String outputDir, String experimentName, Logger logger) {
+        this.gtfFile = gtfFile;
+        this.ipBam = ipBam;
+        this.inputBam = inputBam;
+        this.outputDir = outputDir;
+        this.experimentName = experimentName;
+        this.logger = logger;
+    }
 
     /**
      * entrance of the program
@@ -16,7 +42,7 @@ public class peakCaller {
         Options options = new Options();
         CommandLine commandLine = setCommandline(args, options);
         String ip, input, gtfFile, outputDir = System.getProperty("user.dir"), experimentName = "m6aPeak";
-        Logger log = null;
+        Logger log;
 
         ip = commandLine.getOptionValue("ip");
         input = commandLine.getOptionValue("input");
@@ -27,78 +53,90 @@ public class peakCaller {
             experimentName = commandLine.getOptionValue("exp");
 
         log = initLog(outputDir);
-        peakCalling(gtfFile, outputDir, ip, input, experimentName, log);
+        File output = new File(outputDir);
+        if (!output.exists()) {
+            boolean res = output.mkdirs();
+            if (!res) {
+                log.error("can not make directory " + outputDir);
+                System.exit(2);
+            }
+        } else {
+            if (output.isFile()) {
+                log.error(output + " should be a directory not a file");
+                System.exit(2);
+            }
+        }
+
+        PeakCaller pc = new PeakCaller(gtfFile, ip, input, outputDir, experimentName, log);
+        pc.peakCalling();
     }
 
     /**
      * peak calling with metpeak in R script
-     * @param gtfFilePath GTF file path
-     * @param outputDirPath name of output result directory
      */
-    public static void peakCalling(String gtfFilePath, String outputDirPath, String IPFile, String INPUTFile, String experimentName , Logger log) {
+    public void peakCalling() {
 
-        File rScript = new File(outputDirPath, "peakCalling.R");
-        boolean gen = generateRScript(rScript, gtfFilePath, IPFile, INPUTFile, experimentName);
+        File rScript = new File(this.outputDir, "peakCalling.R");
+        boolean gen = generateRScript(rScript);
         if (!gen) {
-            log.error(" R script can not generate or set to be executable");
+            this.logger.error(" R script can not generate or set to be executable");
             System.exit(2);
         }
         try {
             Process p = Runtime.getRuntime().exec("chmod a+x " + rScript.getAbsolutePath());
             int res = p.waitFor();
             if (res != 0) {
-                log.error("chmod failed");
+                this.logger.error("chmod failed");
                 System.exit(2);
             }
         } catch (IOException | InterruptedException ie) {
-            log.error(ie.getMessage());
+            this.logger.error(ie.getMessage());
             System.exit(2);
         }
-        String[] params = new String[]{"Rscript", rScript.getAbsolutePath()};
-        String cmd = String.join(" ", params);
-        log.debug(cmd);
-        String finalOutput = new File(outputDirPath, experimentName).getAbsolutePath();
-        log.debug("peak calling procedure, may take a little bit long time. Result output in: " + finalOutput);
+
+        RConnection rc = null;
+        REXP x = null;
         try {
-            Process p = Runtime.getRuntime().exec(cmd);
-            int res = p.waitFor();
-            if (res != 0) {
-                log.error("Run peak calling failed");
-                System.exit(2);
-            }
-        } catch (IOException | InterruptedException ie) {
-            log.error(ie.getMessage());
+            rc = new RConnection();
+            rc.eval("source('" + rScript + "')");
+            this.logger.debug("start peak calling...");
+            String finalOutput = new File(this.outputDir, this.experimentName).getAbsolutePath();
+            this.logger.debug("peak calling procedure, may take a little bit long time. Result output in: " + finalOutput);
+            rc.eval("peakCall()");
+            rc.close();
+        } catch (RserveException e) {  // | REXPMismatchException
+            this.logger.error(e.getMessage());
             System.exit(2);
         }
-        log.debug("peak calling complete");
+        this.logger.debug("peak calling complete");
+
+        boolean res = rScript.delete();
+        if (!res)
+            this.logger.error("can not delete redundant file " + rScript);
     }
 
     /**
      * generate R script for MeTPeak peak calling
-     * @param rScript R script file path
-     * @param gtfFilePath GTF file path
-     * @param IPFile IP bam file
-     * @param INPUTFile INPUT bam file
-     * @param experimentName output name
+     * @param rScriptFilePath R script file path
      */
-    private static boolean generateRScript(File rScript, String gtfFilePath, String IPFile, String INPUTFile, String experimentName) {
+    private boolean generateRScript(File rScriptFilePath) {
         boolean res;
         try {
-            if (!rScript.exists()) {
-                res = rScript.createNewFile();
+            if (!rScriptFilePath.exists()) {
+                res = rScriptFilePath.createNewFile();
                 if (!res) {
                     return res;
                 }
             }
 
             BufferedWriter bfw = new BufferedWriter(
-                    new OutputStreamWriter(new FileOutputStream(rScript, false))
+                    new OutputStreamWriter(new FileOutputStream(rScriptFilePath, false))
             );
 
             bfw.write("library(\"MeTPeak\")");
             bfw.newLine();
-            bfw.write("metpeak(GENE_ANNO_GTF=\"" + gtfFilePath + "\",IP_BAM=\"" + IPFile + "\", INPUT_BAM=\"" + INPUTFile +
-                           "\", OUTPUT_DIR=\"" + rScript.getParent() + "\", EXPERIMENT_NAME=\"" + experimentName + "\")");
+            bfw.write("peakCall <- function() {\n\tmetpeak(GENE_ANNO_GTF=\"" + this.gtfFile + "\",IP_BAM=\"" + this.ipBam + "\", INPUT_BAM=\"" + this.inputBam +
+                           "\", OUTPUT_DIR=\"" + this.outputDir + "\", EXPERIMENT_NAME=\"" + this.experimentName + "\")\n}");
             bfw.newLine();
             bfw.flush();
             bfw.close();
@@ -106,7 +144,7 @@ public class peakCaller {
             ie.getMessage();
             System.exit(2);
         }
-        res = rScript.setExecutable(true);
+        res = rScriptFilePath.setExecutable(true);
 
         return res;
     }
@@ -116,7 +154,7 @@ public class peakCaller {
      * @param args input parameters
      * @param options Options instance
      * @return CommandLineParser
-     * @throws ParseException
+     * @throws ParseException parseException
      */
     private static CommandLine setCommandline(String[] args, Options options) throws ParseException {
 
@@ -151,6 +189,6 @@ public class peakCaller {
      */
     private static Logger initLog(String logHome) {
         System.setProperty("log_home", logHome);
-        return Logger.getLogger(peakCaller.class);
+        return Logger.getLogger(PeakCaller.class);
     }
 }
